8638c106ac13ef32e95a8719be68190c3a13c23e
khadijaamjad <khadijah.amjad25@gmail.com>
2024-03-27 03:46:39 +0500

    Changed start screen to show steps to play. Updated icon.

--- type-sprint/src/components/index.ts
+++ type-sprint/src/components/index.ts
@@ -1,11 +1,13 @@
 import { lazy } from "react";
 
 const StartPrompt = lazy(() => import("./StartPrompt/StartPrompt"));
+const HowToPlay = lazy(() => import("./HowToPlay/HowToPlay"));
 const DarkModeButton = lazy(() => import("./DarkModeButton/DarkModeButton"));
 import GameScreen from "./GameScreen/GameScreen";
 import FinalScore from "./FinalScore/FinalScore";
 import ScoreDetail from "./ScoreDetail/ScoreDetail";
 import GradientButton from "./GradientButton/GradientButton";
+import Step from "./Step/Step";
 
 export {
   StartPrompt,
@@ -13,5 +15,7 @@ export {
   ScoreDetail,
   GradientButton,
   GameScreen,
-  DarkModeButton
+  DarkModeButton,
+  Step,
+  HowToPlay
 };
